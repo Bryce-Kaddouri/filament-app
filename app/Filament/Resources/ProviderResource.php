36624dd315a751ad0208a04be2d4d4e1bea8e3a3
Bryce Kaddouri <bryce.kaddouri@mycloudpa.com>
2024-11-13 17:28:19 +0000

    add map for provider

--- app/Filament/Resources/ProviderResource.php
+++ app/Filament/Resources/ProviderResource.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ProviderResource\Pages;
 use App\Filament\Resources\ProviderResource\RelationManagers;
 use App\Models\Provider;
+use CodeWithDennis\SimpleMap\Components\Forms\SimpleMap as FormsSimpleMap;
+use CodeWithDennis\SimpleMap\SimpleMap;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -12,6 +14,11 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Tapp\FilamentGoogleAutocomplete\Forms\Components\GoogleAutocomplete;
+use Cheesegrits\FilamentGoogleMaps\Fields\Map;
+use Cheesegrits\FilamentGoogleMaps\Columns\MapColumn;
+
+use Filament\Forms\Components\Fieldset;
 
 class ProviderResource extends Resource
 {
@@ -30,6 +37,8 @@ class ProviderResource extends Resource
 
     public static function form(Form $form): Form
 {
+
+  
     return $form
         ->schema([
             Forms\Components\TextInput::make('name')
@@ -43,40 +52,93 @@ class ProviderResource extends Resource
                 ->tel()
                 ->required()
                 ->maxLength(255),
-            Forms\Components\TextInput::make('address')
+             // Color Picker
+             Forms\Components\ColorPicker::make('color')
+             ->required()
+             ->reactive()
+             ->afterStateHydrated(function ($component, $record) {
+                 if ($record) {
+                     $hexColor = sprintf("#%02x%02x%02x", $record->red, $record->green, $record->blue);
+                     $component->state($hexColor);
+                 }
+             })
+             ->afterStateUpdated(function (callable $set, $state) {
+                 $red = hexdec(substr($state, 1, 2));
+                 $green = hexdec(substr($state, 3, 2));
+                 $blue = hexdec(substr($state, 5, 2));
+                 $set('red', $red);
+                 $set('green', $green);
+                 $set('blue', $blue);
+             }),
+
+         // Hidden inputs to hold RGB values
+         Forms\Components\Hidden::make('red'),
+         Forms\Components\Hidden::make('green'),
+         Forms\Components\Hidden::make('blue'),
+                Forms\Components\FileUpload::make('image')
+                ->columnSpan(2)
+                ->image(),
+        
+            Fieldset::make('Address')
+            /* ->afterStateUpdated(function($state, $set){
+                dd($state, $set);
+            }) */
+            ->schema([
+            Forms\Components\TextInput::make('full_address')
+                ->columnSpan(2)
                 ->required()
                 ->maxLength(255),
-            Forms\Components\FileUpload::make('image')
-                ->image(),
-            
-            // Color Picker
-            Forms\Components\ColorPicker::make('color')
+                Forms\Components\TextInput::make('street')
                 ->required()
-                ->reactive()
-                ->afterStateHydrated(function ($component, $record) {
-                    if ($record) {
-                        $hexColor = sprintf("#%02x%02x%02x", $record->red, $record->green, $record->blue);
-                        $component->state($hexColor);
-                    }
-                })
-                ->afterStateUpdated(function (callable $set, $state) {
-                    $red = hexdec(substr($state, 1, 2));
-                    $green = hexdec(substr($state, 3, 2));
-                    $blue = hexdec(substr($state, 5, 2));
-                    $set('red', $red);
-                    $set('green', $green);
-                    $set('blue', $blue);
-                }),
+                ->readOnly()
+                ->maxLength(255),
+                Forms\Components\TextInput::make('city')
+                ->required()
+                ->readOnly()
+                ->maxLength(255),
+                Forms\Components\TextInput::make('state')
+                ->required()
+                ->readOnly()
+                ->maxLength(255),
+                Forms\Components\TextInput::make('zip')
+                ->required()
+                ->readOnly()
+                ->maxLength(255),
+                Map::make('location')
+                ->mapControls([
+                    'mapTypeControl'    => false,
+                    'scaleControl'      => true,
+                    'streetViewControl' => false,
+                    'rotateControl'     => false,
+                    'fullscreenControl' => false,
+                    'searchBoxControl'  => false, // creates geocomplete field inside map
+                    'zoomControl'       => true,
+                ])
+                
+                ->defaultZoom(15) // default zoom level when opening form
+                ->autocomplete('full_address') // field on form to use as Places geocompletion field
+                ->autocompleteReverse(true)
+                ->columnSpan(2) // reverse geocode marker location to autocomplete field
+                ->reverseGeocode([
+                    'street' => '%n %S',
+                    'city' => '%L',
+                    'state' => '%A1',
+                    'zip' => '%z',
+                ]) // reverse geocode marker location to form fields, see notes below
+                ->debug() // prints reverse geocode format strings to the debug console 
+               // ->defaultLocation([39.526610, -107.727261]) // default for new forms
+                ->draggable(false) // allow dragging to move marker
+                ->clickable(false) // allow clicking to move marker
+                ->geolocate(true) // adds a button to request device location and set map marker accordingly
+                ->geolocateLabel('Get Location') // overrides the default label for geolocate button
+                ->geolocateOnLoad(false, false), // geolocate on load, second arg 'always' (default false, only for new form))
+                ])
+                ->columnSpan(2),
+               
+                
+                
 
-            // Hidden inputs to hold RGB values
-            Forms\Components\Hidden::make('red'),
-            Forms\Components\Hidden::make('green'),
-            Forms\Components\Hidden::make('blue'),
-            Forms\Components\Select::make('products')
-                ->relationship('products', 'name')
-                ->multiple()
-                ->preload()
-                ->disabled(true),
+            
         ]);
 }
 
@@ -84,6 +146,7 @@ class ProviderResource extends Resource
 
     public static function table(Table $table): Table
     {
+        
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
@@ -94,8 +157,17 @@ class ProviderResource extends Resource
                     ->sortable(),
                 Tables\Columns\TextColumn::make('phone')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('address')
-                    ->searchable(),
+                    MapColumn::make('location')
+                    ->extraAttributes([
+                      'class' => 'my-funky-class'
+                    ]) // Optionally set any additional attributes, merged into the wrapper div around the image tag
+                   
+                    ->height(height: '150') // API setting for map height in PX
+                    ->width(width: '150') // API setting got map width in PX
+                    ->type('roadmap') // API setting for map type (hybrid, satellite, roadmap, tarrain)
+                    ->zoom(10) // API setting for zoom (1 through 20)
+                    ->ttl(60 * 60 * 24 * 30), // number of seconds to cache image before refetching from API
+                
                 Tables\Columns\ImageColumn::make('image'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
