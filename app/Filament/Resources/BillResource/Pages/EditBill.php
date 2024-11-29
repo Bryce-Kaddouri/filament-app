2bf4970dc67bd264cf317fc3307124290724e781
Bryce Kaddouri <bryce.kaddouri@mycloudpa.com>
2024-11-29 13:27:29 +0000

    start to add for edit but need to add line_items in db

--- app/Filament/Resources/BillResource/Pages/EditBill.php
+++ app/Filament/Resources/BillResource/Pages/EditBill.php
@@ -3,9 +3,11 @@
 namespace App\Filament\Resources\BillResource\Pages;
 
 use App\Filament\Resources\BillResource;
+use App\Services\ParsedImage;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class EditBill extends EditRecord
 {
@@ -23,5 +25,33 @@ class EditBill extends EditRecord
         dd($data);
         $record->update($data);
         return $record;
+    }
+
+    public function mount($record): void
+    {
+        $this->previousUrl = url()->previous();
+        $this->record = $this->resolveRecord($record);
+
+        $this->authorizeAccess();
+
+        $documentJson = Storage::get('bills/' . $this->record->id . '/json_document.json');
+        //dd(json_decode($documentJson, true)   );
+        $parsedImage = ParsedImage::fromJson($documentJson);
+        $dataForFrontend = $parsedImage->toJsonSerializable();
+       //  dd($parsedImage);
+
+            $this->form->fill(
+                [
+                    'provider_id' => $this->record->provider_id,
+                    'bill_number' => $this->record->bill_number,
+                    'bill_date' => $this->record->bill_date,
+                    'file_url' => $this->record->file_url,
+                    'json_document' => $documentJson,
+                    'line_items' => $parsedImage->getLineItems(),
+                    'generated_data' => $dataForFrontend,
+                    'data_for_img' => $dataForFrontend
+                ]
+            );
+        
     }
 }
